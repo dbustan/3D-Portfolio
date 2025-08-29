ebdb833f942f70ee51f417a4c6193d71c56624db
dbustan <danielbustan100@gmail.com>
2025-08-28 21:06:14 -0700

    finished?4

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -111,13 +111,13 @@ const Projects = () => {
                         <ProjectLink  href= "https://store.steampowered.com/app/2928200/Permanence_Break/" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none', color: 'inherit' }}>
                         {/* Example entry for a game */}
                         <ProjectItem>
-                            <ProjectImage src="./permanenceicon.png" alt="Game 1" />
+                            <ProjectImage src="/3D-Portfolio/permanenceicon.png" alt="Game 1" />
                             <ProjectName>Permanence Break</ProjectName>
                         </ProjectItem>
                         </ProjectLink>
                         <ProjectLink  href= "https://shadowhandstudios.itch.io/storm-base" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none', color: 'inherit' }}>
                         <ProjectItem>
-                            <ProjectImage src="./sb.png" alt="Game 2" />
+                            <ProjectImage src="/3D-Portfolio/sb.png" alt="Game 2" />
                             <ProjectName>StormBase</ProjectName>
                         </ProjectItem>
                         </ProjectLink>
@@ -137,7 +137,7 @@ const Projects = () => {
                         </ProjectLink>
                         <ProjectLink  href= "https://www.youtube.com/watch?v=j1HqWT4MG08&pp=ygUWbGV2ZWwgIHN0YXJ0IGNvb2xudXNoaQ%3D%3D" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none', color: 'inherit' }}>
                         <ProjectItem>
-                            <ProjectImage src="./3D-Portfolio/levelstart.png" alt="Song 2" />
+                            <ProjectImage src="/3D-Portfolio/levelstart.png" alt="Song 2" />
                             <ProjectName>Level Start</ProjectName>
                         </ProjectItem>
                         </ProjectLink>
